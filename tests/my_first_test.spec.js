3873fab8f25b8ba8829ccb889f33334894310996
KngCd <panaligankingcedrick@gmail.com>
2026-06-27 20:57:36 +0800

    Update test to navigate to Google and verify the title

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('My first test', async ({page}) => {
-    await page.goto('https://kuboware.com');
+    await page.goto('https://google.com');
 
-    await expect(page).toHaveTitle(/Kubo-ware/);
+    await expect(page).toHaveTitle(/Google/);
 });
