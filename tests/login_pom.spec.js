7e5a7debc3629909a1cdd70523d5e20550716041
KngCd <panaligankingcedrick@gmail.com>
2026-07-04 21:16:39 +0800

    Add LoginPage class and login test implementation

---
+++ tests/login_pom.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/login';
+
+test('Login', async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+
+    await loginPage.gotoLoginPage();
+    await loginPage.login(process.env.APP_USERNAME, process.env.APP_PASSWORD);
+});
